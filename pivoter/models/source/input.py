52ea942911dc2e7d871a60debc289a5a2aa71f6a
Michael Adams <michaeladams@Michaels-MacBook-Pro.local>
2022-06-13 15:33:46 +0100

    more notes

--- pivoter/models/source/input.py
+++ pivoter/models/source/input.py
@@ -42,16 +42,22 @@ class Input:
     had_initial_path: Optional[Path]
     tables: Optional[List[LiveTable]]
 
-    """
-    Dev notes:
 
-    - At any given time the user will be utilising a single LiveTable.
-    - A class:LiveTable consist of two class:Table's as follow:
-        - pristine: all the cells the table had when loaded in, this is never modified.
-        - filtered: the users current selection
-    
-    The following methods allow for iteration methods that will seem natural to the user
-    """
+    @property
+    def name(self) -> str:
+        """
+        Get name of currently selected table
+        """
+        return self.selected_table.name
+
+
+    @property
+    def title(self) -> str:
+        """
+        Alternate call to name.
+        """
+        return self.name
+
 
     def __iter__(self):
         if self.is_singelton_table:
@@ -62,26 +68,18 @@ class Input:
             yield self
 
     """
+    Dev notes:
+
+    - At any given time the user will be utilising a single LiveTable.
+    - A class:LiveTable consist of two class:Table's as follow:
+        - pristine: all the cells the table had when loaded in, this is never modified.
+        - filtered: the users current selection
+    
+    The following methods allow for iteration methods that will seem natural to the user
     The following methods all do one of two things:
     1.) Expand the current selection by comparing ".filtered" to ".pristine".
     2.) Filter down ".filtered" to a smaller selection.
-
     """
-
-    @property
-    def name(self) -> str:
-        """
-        Get name of currently selected table
-        """
-        return self.selected_table.name
-
-    @property
-    def title(self) -> str:
-        """
-        Alternate call to name.
-        """
-        return self.name
-
 
     def excel_ref(self, excel_ref: str):
         """
@@ -92,6 +90,7 @@ class Input:
         wanted_cells: List[BaseCell] = cellutils.get_ref_as_wanted_basecells(excel_ref)
         self.selected_table.filtered._filter_to_matching_xys(wanted_cells)
         return self
+
 
     def lone_value(self) -> str:
         """
