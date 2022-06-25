74ac5335d8fd552f5f6804ed1144c567909f6c67
mikeAdamss <mikelovesbooks@gmail.com>
2022-06-25 17:39:10 +0100

    tidying up

--- pivoter/utils/cellutils/__init__.py
+++ pivoter/utils/cellutils/__init__.py
@@ -1,3 +1,9 @@
-from .excel import (excel_ref_as_wanted_basecells, letter_to_x,
-                    multi_excel_ref_to_basecells, number_to_y,
-                    single_excel_ref_to_basecells, x_to_letters, y_to_number)
+from .excel import (
+    excel_ref_as_wanted_basecells,
+    letter_to_x,
+    multi_excel_ref_to_basecells,
+    number_to_y,
+    single_excel_ref_to_basecells,
+    x_to_letters,
+    y_to_number,
+)
--- tests/unit/selections/selectable/test_fill.py
+++ tests/unit/selections/selectable/test_fill.py
@@ -18,15 +18,15 @@ def test_expand(selectable_simple1: Selectable):
     s = selectable_simple1.excel_ref("W5:W10").fill(DOWN)
     assert len(s.cells) == 90
 
-    s = selectable_simple1.excel_ref("E2").fill(
-        DOWN
-    ) - selectable_simple1.excel_ref("E4").fill(DOWN)
+    s = selectable_simple1.excel_ref("E2").fill(DOWN) - selectable_simple1.excel_ref(
+        "E4"
+    ).fill(DOWN)
     assert len(s.cells) == 2
 
     s = selectable_simple1.excel_ref("W75").fill(RIGHT).fill(UP)
     assert len(s.cells) == 222
 
-    s = selectable_simple1.excel_ref("D4").fill(
-        LEFT
-    ) - selectable_simple1.excel_ref("Z4").fill(LEFT)
+    s = selectable_simple1.excel_ref("D4").fill(LEFT) - selectable_simple1.excel_ref(
+        "Z4"
+    ).fill(LEFT)
     assert len(s.cells) == 0
