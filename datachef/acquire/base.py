1cc2cc1bab31b3e7a9eeadc4c95f08a7b92b427a
mikeAdamss <mikelovesbooks@gmail.com>
2023-07-22 18:50:50 +0100

    more documentation

--- datachef/acquire/base.py
+++ datachef/acquire/base.py
@@ -9,14 +9,6 @@ class BaseReader(metaclass=ABCMeta):
     """
     Baseclass that all readers inherit from.
     """
-
-    # Note - we attach tables to the reader for a reason.
-    # Some (but not all) table reading libraries allow you
-    # to only read in certain sheets (rather than our default
-    # of filtering out after reading).
-    # Where this is possible this allows us to hand the filtering
-    # of tables names over to the reader (which can then None this
-    # property).
     tables: Optional[str] = None
 
     @abstractmethod
